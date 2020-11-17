90fca84f41f3008ccf2c6eaa6b6c7d018ed709db
Colin Whitney <colin.whitney@gmail.com>
2020-11-17 16:06:56 -0500

    added router to App.js; created Profile component and linked to App through route CW

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Nav from './components/Nav'
+import Profile from './components/Profile'
 
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Nav/>
-        <h1>InstaClone</h1>
-      </div>
+      <Router>
+        <div className="container">
+          <Nav/>
+          <h1>InstaClone</h1>
+          <Route path="/profile" component={Profile}/>
+        </div>
+      </Router>
+      
     )
   }
 }
