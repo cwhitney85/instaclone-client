b0f48675e077e827d7b9491553f017d7a264cc41
Colin Whitney <colin.whitney@gmail.com>
2020-11-17 09:56:12 -0500

    added components folder with a Nav and imported to app.js CW

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Nav from './components/Nav'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Nav/>
         <h1>InstaClone</h1>
       </div>
     )
