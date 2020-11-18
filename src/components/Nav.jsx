bd8a46b0dea1827b6d4d1fb9f86ba64bb800c6e8
Colin Whitney <colin.whitney@gmail.com>
2020-11-18 11:23:27 -0500

    added Link to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Nav extends Component {
   render() {
     return (
       <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-        <h3 className="navbar-brand">InstaClone</h3>
+        <Link to="/">
+          <h3 className="navbar-brand">InstaClone</h3>
+        </Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav flex-row ml-auto d-flex">
-            <li className="nav-item nav-link">Sign up</li>
-            <li className="nav-item nav-link">Log In</li>
+            <Link to="/">
+              <li className="nav-item nav-link">Sign up</li>
+            </Link>
+            <Link to="/">
+              <li className="nav-item nav-link">Log In</li>
+            </Link>
+            <Link to="/profile">
+              <li className="nav-item nav-link">Profile</li>
+            </Link>
           </ul>
         </div>
       </nav>
