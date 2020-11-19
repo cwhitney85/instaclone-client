780b55c7d38f72792f18683f9ce28218e37dbe30
calmworld <sarah.alhuriz@hotmail.com>
2020-11-19 16:58:49 -0700

    SA file upload works now

--- src/components/CreateFeed.jsx
+++ src/components/CreateFeed.jsx
@@ -1,96 +1,54 @@
+import Axios from 'axios'
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import axios from "axios"
-import { toast, ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import {Redirect} from 'react-router-dom'
 
 
 export default class CreateFeed extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            title: "",
-            selectedFile: null,
-            fileName: "",
-            loaded: 0
+            image: "",
+            description: "",
+            tags: "",
+            redirect: false
         }
-        this.onChangeTitle = this.onChangeTitle.bind(this)
+        this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.onChangeUpload = this.onChangeUpload.bind(this)
-        this.onClickUpload = this.onClickUpload.bind(this)
     }
 
-    onChangeTitle(event) {
-        this.setState({ 
-            [event.target.id]: event.target.value
-        })
+    handleChange(event) {
+        console.log(event.target.value)
+        this.setState({ [event.target.name]: event.target.value})
     }
 
     handleSubmit(event) {
-        fetch("/create", {
-            method: "post",
-            body: JSON.stringify({
-                title: this.state.title, 
-                selectedFile: this.state.selectedFile,
-                fileName: this.state.fileName
-            }),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => res.json())
-        .then(data => {
-            this.props.handleAddFeed(data)
-            return (
-                <Redirect to="/feeds"/>
-            )
-        })
+        event.preventDefault()
+        const packageUpload = {
+            image: this.state.image,
+            description: this.state.description,
+            tags: this.state.tags
+        }
+        Axios.post('http://localhost:3003/feeds', packageUpload)
+        .then(this.setState({
+            redirect: true
+        }))
     }
 
-    onChangeUpload(event) {
-        console.log(event.target.files[0])
-            this.setState({ 
-                selectedFile: event.target.files[0],
-                fileName: event.target.files[0].name,
-                loaded: 0
-            })
-    }
-
-    onClickUpload = () => {
-        const data = new FormData()
-        data.append('file', this.state.selectedFile)
-        axios.post("http://localhost:3003/upload", data, { // receive two parameter endpoint url ,form data 
-        }).then(res => { // then print response status
-        console.log(res.statusText)
-        toast.success('upload success')
-        }).catch(err => {
-            toast.error('upload file')
-        })
-    }
     
     render() {
+        if(this.state.redirect) {
+            return <Redirect to={"/"}/>
+        }
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <div className="input-group mb-3">
-                        <div className="custom-file">
-                            <input type="file" name="file" value="" onChange={this.onChangeUpload}className="custom-file-input" id="inputGroupFile01"/>
-                            <label className="custom-file-label" htmlFor="file" name="file" aria-describedby="inputGroupFileAddon01">Choose file</label>
-                        </div>
-                        <div className="input-group-append">
-                            <span onClick={this.onClickUpload} className="input-group-text" id="inputGroupFileAddon01">Upload</span>
-                        </div>
-                        <div className="form-group">
-                        <ToastContainer />
-                        </div>
-                    </div>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text">Title</span>
-                        </div>
-                        <input type="text" onChange={this.onChangeTitle} value={this.state.title} className="form-control"></input>
-                    </div>
-                    <br />
-                    <button className="btn btn-primary" type="submit" value="Submit">Submit</button>
+                    <label className="form-label">Image</label>
+                    <input className="form-input" onChange={this.handleChange} type="text" name="image" id="image" value={this.state.image}/>
+                    <label className="form-label">Description</label>
+                    <input className="form-input" onChange={this.handleChange} type="text" name="description" id="description" value={this.state.description}/>
+                    <label className="form-label">Tags</label>
+                    <input className="form-input" onChange={this.handleChange} type="text" name="tags" id="tags" value={this.state.tags}/>
+                    <button type="submit" className="submit-btn">Submit</button>
                 </form>
             </div>
         )
